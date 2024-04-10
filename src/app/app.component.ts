46a188bb84b6aa36f82ac4c8feacac666fa12fc5
Fernando Paz <ferpaz64@gmail.com>
2024-04-10 16:24:35 -0600

    Contador Simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Primera App de Angular !!';
+
+  public counter: number = 0;
+
+  public increment(value: number): void {
+    this.counter = this.counter + value;
+  }
+
+  public decrement(value: number): void {
+    if ((this.counter - value) >= 0) this.counter = this.counter - value;
+  }
+
+  public reset(): void {
+    this.counter = 0;
+  }
 }
